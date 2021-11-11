37da9277ea1ca113d59ffedf129f734ddeae41ca
suraj <surajkumarsahani1997@gmail.com>
2021-11-11 19:29:10 +0530

    State change

--- app/src/main/java/com/suraj/playo/NewsActivity.kt
+++ app/src/main/java/com/suraj/playo/NewsActivity.kt
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.suraj.playo.adapter.NewsArticlesAdapter
 import com.suraj.playo.ui.base.BaseActivity
 import com.suraj.playo.ui.newslist.NewsViewModel
+import com.suraj.playo.ui.newslist.State
 import com.suraj.playo.ui.newslist.WebViewActivity
 import com.suraj.playo.utils.getViewModel
 import kotlinx.android.synthetic.main.activity_main.*
